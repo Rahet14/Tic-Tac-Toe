4edef38c11ded564440ba9e55feea9be044fb58f
Rahet14 <rahettuteja@gmail.com>
2020-12-23 14:55:28 +0530

    Declare a winner

--- src/Board.js
+++ src/Board.js
@@ -6,14 +6,55 @@ class Board extends Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
+      won: false,
+      winner: null,
     };
   }
 
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   handleClick(i) {
+    if (this.state.won) {
+      return;
+    }
+
     let squares = this.state.squares.slice();
-    squares[i] = "X";
+
+    const xIsNext = this.state.xIsNext;
+    squares[i] = xIsNext ? "X" : "O";
+
+    var winner = this.calculateWinner(squares);
+
+    const won = winner === "X" || winner === "O" ? true : false;
+
     this.setState({
       squares: squares,
+      xIsNext: !xIsNext,
+      won: won,
+      winner: winner,
     });
   }
 
@@ -25,8 +66,11 @@ class Board extends Component {
       />
     );
   }
+
   render() {
-    const status = "Next player: X";
+    const status = this.state.won
+      ? "Winner: " + this.state.winner
+      : "Next player: " + (this.state.xIsNext ? "X" : "O");
     return (
       <div>
         <div className="status">{status}</div>
