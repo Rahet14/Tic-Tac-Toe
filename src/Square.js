cc5d8bc70b61c7d1f513bcb7ea4bc690aca9169c
Rahet14 <rahettuteja@gmail.com>
2020-12-23 14:10:55 +0530

    Add Styles, Game, Board, and Square Component

---
+++ src/Square.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Square extends Component {
+  state = {};
+  render() {
+    return <button className="square">{this.props.value}</button>;
+  }
+}
+
+export default Square;
